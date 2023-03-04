3d973b3ea881a2b2a0f69a92de15f6b7ad933900
Sergey Kukharenko <iamksergey@mail.ru>
2023-03-04 09:39:10 +0300

    Ready to production

--- components/HhData/HhData.tsx
+++ components/HhData/HhData.tsx
@@ -1,5 +1,5 @@
 import { HhDataProps } from './HhData.props';
-import styles from './hhData.module.css';
+import styles from './HhData.module.css';
 import cn from 'classnames';
 import RateIcon from './rate.svg';
 import { Card } from '..';
